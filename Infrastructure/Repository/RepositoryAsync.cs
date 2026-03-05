19b1ca6bb7e65a83d243f49d4dad51380c5c0f87
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 10:45:44 -0600

    feat: add repository async

---
+++ Infrastructure/Repository/RepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Ardalis.Specification.EntityFrameworkCore;
+using Infrastructure.Context;
+
+namespace Infrastructure.Repository;
+
+public class RepositoryAsync<T> : RepositoryBase<T>, IRepositoryAsync<T> where T : class
+{
+    private readonly DatabaseContext dbContext;
+
+    public RepositoryAsync(DatabaseContext dbContext) : base(dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+}

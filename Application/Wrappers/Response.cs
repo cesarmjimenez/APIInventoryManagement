a866b319ac0fc0e3dbe8ae7a7d36d090ed257b55
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 09:40:22 -0600

    feat: add wrapper response

---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,33 @@
+﻿namespace Application.Wrappers;
+
+public class Response<T>
+{
+    public Response()
+    {
+
+    }
+
+    public Response(T data, string? message = null)
+    {
+        Succeeded = true;
+        Message = message ?? string.Empty;
+        Data = data;
+        Errors = [];
+    }
+
+    public Response(string message)
+    {
+        Succeeded = false;
+        Message = message;
+        Data = default!;
+        Errors = [];
+    }
+
+    public bool Succeeded { get; set; }
+
+    public string Message { get; set; }
+
+    public List<string> Errors { get; set; }
+
+    public T Data { get; set; }
+}

7cc9c3d064b79f09547842db632500fca432d518
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 08:33:13 -0600

    feat: add user entity

---
+++ Domain/Entities/Users.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Users : IdBaseEntity
+    {
+        public required string FirstName { get; set; }
+
+        public string? SecondName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public string? SecondLastName { get; set; }
+
+        public required string FullName { get; set; }
+
+        public required string UserName { get; set; }
+
+        public required string PasswordHash { get; set; }
+
+        public required string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public Roles Role { get; set; } = null!;
+
+        public Guid LocationId { get; set; }
+
+        public Locations Location { get; set; } = null!;
+    }
+}

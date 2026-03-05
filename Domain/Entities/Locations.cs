738829334778b4f50c236a40c10d5801e46c3e12
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 08:15:40 -0600

    feat: add locations entity

---
+++ Domain/Entities/Locations.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Locations : IdBaseEntity
+{
+    public required string Name { get; set; }
+
+    public required string Address { get; set; }
+
+    public required string City { get; set; }
+
+    public bool IsWareHouse { get; set; }
+
+    public bool IsActive { get; set; }
+}

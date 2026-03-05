45e63109b04623629abf39c1e8d09d886fc7c724
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 08:41:53 -0600

    feat: add product batches entity

---
+++ Domain/Entities/ProductBatches.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class ProductBatches : IdBaseEntity
+{
+    public required string BatchNumber { get; set; }
+
+    public required DateTime ExpirationDate { get; set; }
+
+    public required decimal Quantity { get; set; }
+
+    public required decimal UnitCost { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public Products Product { get; set; } = null!;
+
+    public Guid LocationId { get; set; }
+
+    public Locations Location { get; set; } = null!;
+}

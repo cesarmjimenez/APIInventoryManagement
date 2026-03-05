4ff554299cc4a88adb6e12ef5c8e3433531f3454
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 10:37:15 -0600

    feat: add IRepositoryAsync

---
+++ Application/Interfaces/IRepositoyAsync.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+
+namespace Application.Interfaces;
+
+public interface IRepositoyAsync<T> : IRepositoryBase<T> where T : class
+{
+}
+
+public interface IReadRepositoryAsync<T> : IReadRepositoryBase<T> where T : class
+{
+}

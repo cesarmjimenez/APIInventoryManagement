f0985d9b189dd8c52e5d866a4d2131f1e7e50b82
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 08:20:28 -0600

    feat: add products entity

---
+++ Domain/Entities/Products.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Products : IdBaseEntity
+{
+    public required string Sku { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Brand { get; set; }
+
+    public bool IsActive { get; set; }
+}

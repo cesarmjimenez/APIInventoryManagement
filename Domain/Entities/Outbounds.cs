2ff7821db198692294c381f983c97a673e63c1e6
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 09:01:09 -0600

    feat: add outbound entity and status enum

---
+++ Domain/Entities/Outbounds.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Common;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Outbounds : IdBaseEntity
+{
+    public required string OutboundNumber { get; set; }
+
+    public StatusEnum Status { get; set; }
+
+    public Guid OriginLocationId { get; set; }
+
+    public Locations OriginLocation { get; set; } = null!;
+
+    public Guid DestinationId { get; set; }
+
+    public Locations Destination { get; set; } = null!;
+
+    public Guid OutboundUserId { get; set; }
+
+    public Users OutboundUser { get; set; } = null!;
+
+    public Guid ReceivedUserId { get; set; }
+
+    public Users ReceivedUser { get; set; } = null!;
+}
---
+++ Domain/Enums/StatusEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Enums
+{
+    public enum StatusEnum
+    {
+        EnviadaASucursal = 0,
+        RecibidoEnSucursal = 1
+    }
+}

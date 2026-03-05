01aaabda144bb4b7be7765e260cf220c3eb36ee2
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 09:09:21 -0600

    feat: add outbound details entity

---
+++ Domain/Entities/OutboundDetails.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class OutboundDetails : IdBaseEntity
+{
+    public required decimal Quantity { get; set; }
+
+    public required decimal SubTotal { get; set; }
+
+    public Guid OutboundId { get; set; }
+
+    public Outbounds Outbound { get; set; } = null!;
+
+    public Guid ProductBatchId { get; set; }
+
+    public ProductBatches ProductBatch { get; set; } = null!;
+}

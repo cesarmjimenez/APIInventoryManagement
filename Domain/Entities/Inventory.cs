4d25031a821296f149d4e5c1020a04341f4c2329
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 08:45:38 -0600

    feat: add inventory entity

---
+++ Domain/Entities/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Inventory : IdBaseEntity
+{
+    public required decimal StockQuantity { get; set; }
+
+    public Guid LocationId { get; set; }
+
+    public Locations Location { get; set; } = null!;
+
+    public Guid ProductBatchId { get; set; }
+
+    public ProductBatches ProductBatch { get; set; } = null!;
+}

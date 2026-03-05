47db9e9a166da2334e2392220492fd77ff09e4a4
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-05 10:28:35 -0600

    feat: add Database Context

---
+++ Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
+    public DbSet<Inventory> Inventory { get; set; }
+
+    public DbSet<Locations> Locations { get; set; }
+
+    public DbSet<OutboundDetails> OutboundDetails { get; set; }
+
+    public DbSet<Outbounds> Outbounds { get; set; }
+
+    public DbSet<ProductBatches> ProductBatches { get; set; }
+
+    public DbSet<Products> Products { get; set; }
+
+    public DbSet<Roles> Roles { get; set; }
+
+    public DbSet<Users> Users { get; set; }
+}

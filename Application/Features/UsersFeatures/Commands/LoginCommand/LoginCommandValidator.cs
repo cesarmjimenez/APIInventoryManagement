2197bbfefcdfcc7a973220effa295190f2b8ef78
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-07 11:45:11 -0600

    feat: add login command validator

--- Application/Features/UsersFeatures/Commands/LoginCommand/LoginCommandValidator.cs
+++ Application/Features/UsersFeatures/Commands/LoginCommand/LoginCommandValidator.cs
@@ -1,5 +1,15 @@
-﻿namespace Application.Features.UsersFeatures.Commands.LoginCommand;
+﻿using FluentValidation;
 
-public class LoginCommandValidator
+namespace Application.Features.UsersFeatures.Commands.LoginCommand;
+
+public class LoginCommandValidator : AbstractValidator<LoginCommand>
 {
+    public LoginCommandValidator()
+    {
+        RuleFor(u => u.UserName)
+            .NotEmpty().WithMessage("El nombre de usuario es requerido");
+
+        RuleFor(u => u.PasswordHash)
+            .NotEmpty().WithMessage("La contraseña es requerida");
+    }
 }

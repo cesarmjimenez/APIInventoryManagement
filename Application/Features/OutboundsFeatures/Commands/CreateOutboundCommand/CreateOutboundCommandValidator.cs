7a1ff57b0adae7f1e4fecebe36babe0369e98f19
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-07 20:51:04 -0600

    feat: add create outbound command validator

--- Application/Features/OutboundsFeatures/Commands/CreateOutboundCommand/CreateOutboundCommandValidator.cs
+++ Application/Features/OutboundsFeatures/Commands/CreateOutboundCommand/CreateOutboundCommandValidator.cs
@@ -1,5 +1,26 @@
-﻿namespace Application.Features.OutboundsFeatures.Commands.CreateOutboundCommand;
+﻿using FluentValidation;
 
-public class CreateOutboundCommandValidator
+namespace Application.Features.OutboundsFeatures.Commands.CreateOutboundCommand;
+
+public class CreateOutboundCommandValidator : AbstractValidator<CreateOutboundCommand>
 {
+    public CreateOutboundCommandValidator()
+    {
+        RuleFor(o => o.DestinationId)
+            .NotEmpty().WithMessage("El destino es obligatorio.")
+            .NotEqual(Guid.Empty).WithMessage("El Id de destino no es valido");
+
+        RuleFor(o => o.Items)
+            .NotNull().WithMessage("La salida necesita productos.")
+            .Must(items => items != null && items.Any()).WithMessage("Debe incluir al menos un item.");
+
+        RuleForEach(o => o.Items).ChildRules(item =>
+        {
+            item.RuleFor(i => i.ProductId)
+            .NotEmpty().WithMessage("El id del producto es obligatorio.");
+
+            item.RuleFor(i => i.Quantity)
+            .GreaterThan(0).WithMessage("Debe incluir al menos un producto.");
+        });
+    }
 }
